8ddc456e3e8045e9f562acb3d9ddffcfe64c6507
Ancoletylic1 <96715910+Ancoletylic1@users.noreply.github.com>
2021-12-27 13:58:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "I Love You",
+    introTitle: 'Mèo con',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    btnIntro: 'next',
+    title: 'Tình yêu a dành cho e như số thập phân vô hạng tuần hoàn: bởi vì nó vô tận',
+    desc: 'Làm bạn gái anh nha',
+    btnYes: 'Đồng ý',
+    btnNo: 'Không nhaaaaaaaa',
+    question: 'Đoán thử lý do tại sao a thích e xem',
+    btnReply: 'Gửi',
+    reply: 'Bởi e đáng yêu, dễ thương, xinh đẹp nhất hệ mặt trời',
+    mess: 'Còn phía sau nữa nhe',
+    messDesc: 'Cố mà giải nốt : )).',
+    btnAccept: 'Next',
+    messLink: 'https://www.mediafire.com/view/hai9cql8kslhzow/Untitled.png/file' //link mess của các bạn. VD: https://m.me/nam.nodemy
+} 
